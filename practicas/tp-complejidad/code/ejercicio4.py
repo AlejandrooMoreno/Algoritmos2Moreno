8302959ceac774d88080fd5caf7b2df5bd743b1c
AlejandrooMoreno <alemoreno2538@gmail.com>
2024-04-13 21:42:59 -0300

    agrego el ejercicio 4

---
+++ practicas/tp-complejidad/code/ejercicio4.py
@@ -0,0 +1,12 @@
+def mitadmenores(L):
+    if len(L) > 2:
+        L.sort()
+        tamaño = len(L)
+        if len(L) % 2 == 0:
+            element = L[tamaño - 2]
+            L.pop(tamaño - 2)
+        else:
+            element = L[tamaño - 1]
+            L.pop()
+        L.insert(round(tamaño/2) - 1, element)
+        return L
