927819cfb8dca7a336be08cec363f9d1d9088287
Alejandro Moreno Robles <162379150+AlejandrooMoreno@users.noreply.github.com>
2024-04-26 20:22:23 +0000

    ahora diccionario tiene Nones, ajuste de las funciones

--- practicas/tp-hashtable/code/dictionary.py
+++ practicas/tp-hashtable/code/dictionary.py
@@ -1,12 +1,16 @@
 class dictionary:
-    def __init__(self, length=9):
-        self.dictionary = [[] for _ in range(length)]
+    def __init__(self):
+        self.size = 9
+        self.elements = [None] * self.size
 
     def __getitem__(self, key):
-        return self.dictionary[key]
+        return self.elements[key]
+
+    def __setitem__(self, key, value):
+        self.elements[key] = value
 
     def __str__(self):
-        return str([item for item in self.dictionary])
+        return str(self.elements)
 
 class dicNode:
   value = None
@@ -19,20 +23,27 @@ def insert(D, key, value):
   newnode = dicNode()
   newnode.value = value
   newnode.key = key
-  D[H(key)].append(newnode)
+  position = H(key)
+  if D[position] == None:
+    newlist = [newnode]
+    D[position] = newlist
+  else:
+    D[position].append(newnode)
   return D
 
 def search(D,key):
   position = H(key)
-  for x in range(0, len(D[position])):
-    if D[position][x].key == key:
-      return D[position][x].value
+  if D[position] != None:
+    for x in range(0, len(D[position])):
+      if D[position][x].key == key:
+        return D[position][x].value
   return None
 
 def delete(D,key):
   position = H(key)
-  for x in range(0, len(D[position])):
-    if D[position][x].key == key:
-      D[position].pop(x)
-      return D
+  if D[position] != None:
+    for x in range(0, len(D[position])):
+      if D[position][x].key == key:
+        D[position].pop(x)
+        return D
   return D
