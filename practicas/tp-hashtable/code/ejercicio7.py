3fe256ca991123eeef9adb320d25dacda5709e07
AlejandrooMoreno <alemoreno2538@gmail.com>
2024-04-30 13:17:03 -0300

    ejercicio 7

---
+++ practicas/tp-hashtable/code/ejercicio7.py
@@ -0,0 +1,16 @@
+def comprimir_cadena(palabra):
+  palabra_comprimida = palabra[0]
+  ultima_letra = palabra[0]
+  repetido = 1
+  for x in range(1, len(palabra)):
+    if palabra[x] == ultima_letra:
+      repetido += 1
+    else:
+      palabra_comprimida = palabra_comprimida + str(repetido) + palabra[x]
+      ultima_letra = palabra[x]
+      repetido = 1
+  palabra_comprimida = palabra_comprimida + str(repetido)
+  if len(palabra_comprimida) < len(palabra):
+    return palabra_comprimida
+  else:
+    return palabra
