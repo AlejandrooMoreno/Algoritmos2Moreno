ace0085706d8f2100ea74d39bc0333b2cf6f4175
AlejandrooMoreno <alemoreno2538@gmail.com>
2024-04-30 09:58:51 -0300

    ejercicio6

---
+++ practicas/tp-hashtable/code/ejercicio6.py
@@ -0,0 +1,10 @@
+import math
+
+def h(codigo_postal):
+  key = ord(codigo_postal(0))
+  for x in range(1, 5):
+    key = int(codigo_postal(x))
+  for x in range(5, 8):
+    key = ord(codigo_postal(x))
+  A = (5^(1/2) - 1) / 2
+  return math.floor(5314410000 * (key * A - math.floor(key * A)))
