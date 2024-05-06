e899e0c740b09d1b3f31a679ef9f365359b2b544
AlejandrooMoreno <alemoreno2538@gmail.com>
2024-05-06 09:10:39 -0300

    corrijo error en ej 6

--- practicas/tp-hashtable/code/ejercicio6.py
+++ practicas/tp-hashtable/code/ejercicio6.py
@@ -3,8 +3,8 @@ import math
 def h(codigo_postal):
   key = ord(codigo_postal(0))
   for x in range(1, 5):
-    key = int(codigo_postal(x))
+    key = key + int(codigo_postal(x))
   for x in range(5, 8):
-    key = ord(codigo_postal(x))
+    key = key + ord(codigo_postal(x))
   A = (5^(1/2) - 1) / 2
   return math.floor(1328602500 * (key * A - math.floor(key * A)))
